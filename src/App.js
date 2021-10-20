7481d0df0bbb229a09d1abbcab1b84b652a0dd4d
ZaidYounan <zaid.younan@gmail.com>
2021-10-20 23:34:04 +1100

    Demonstrate React Hooks for students

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [contactList, setContactList] = useState([]);
+
+  
+  function handleClick() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(
+      (result) => {
+        setContactList(result);
+      },
+      (error) => {
+        console.log('error')
+      }
+    )
+  }
+
   return (
     <div className="App">
-        Hello, world.
-        My names Jeff.
+      <button onClick={handleClick}>Load Contacts</button>
+      {contactList ? (contactList.map((data) => (
+        <div className="contact" key={data.id}>
+          <p>{data.name}</p>
+          <p>{data.email}</p>
+          <p>{data.phone}</p>
+        </div>
+      ))) : (<h1>Loading</h1>)}
+      {console.log(contactList)}
     </div>
   );
 }
